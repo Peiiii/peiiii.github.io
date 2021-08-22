1f0308ffd78bd844a3b9cef02b23205f85026112
Peiiii <42300463+Peiiii@users.noreply.github.com>
2021-08-22 23:51:59 +0800

    add pwa support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -122,6 +122,34 @@ module.exports = {
         // ```
         // When applying `zh` in language, please install `nodejieba` in your project.
       }
+    ],
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString"
+        ],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/docusaurus.png"
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json" // your PWA manifest
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)"
+          }
+        ]
+      }
     ]
   ]
 };
